004c8dcc4ab96bab650f35ce5b1461bb1d0d42eb
Joanna Rostek <joannadrostek@gmail.com>
2020-03-05 15:28:42 +0100

    working version, though not very well

--- src/color_refinement.py
+++ src/color_refinement.py
@@ -2,43 +2,41 @@ import itertools as iter
 
 from src.graph_io import *
 
-
-def degree(v):
-    deg = 0
-    for _ in v.incidence:
-        deg += 1
-    return deg
-
-
 with open("../examplegraph.gr") as f:
     G = load_graph(f)
     n = len(G.vertices)
-    num_of_vertices_of_color = [None] * n
+    num_of_vertices_of_color = [0] * n
     color_dicts = []
+    changed = True
 
     # initial coloring - each vertex by degree
     for v in G.vertices:
-        v.degree = degree(v)
         for i in range(1, n - 1):
             if v.degree == i:
-                v.color = i
+                v.colornum = i
                 num_of_vertices_of_color[i] += 1
 
-    # loop over the vertices of same color
-    for l in num_of_vertices_of_color:
-        if l > 1:
-            v = G.vertices[l]
-            same_color_neighbours = {}
-            for e in G.incidence:
-                w = e.other_end(v)
-                w_color = w.color
-                same_color_neighbours[w_color] += 1
-            color_dicts.append(same_color_neighbours)
+    while changed:
+        changed = False
+        # loop over the vertices of same color
+        for l in num_of_vertices_of_color:
+            if l > 1:
+                v = G.vertices[l]
+                same_color_neighbours = {}
+                for e in v.incidence:
+                    w = e.other_end(v)
+                    if w.colornum not in same_color_neighbours:
+                        same_color_neighbours[w.colornum] = 1
+                    else:
+                        same_color_neighbours[w.colornum] += 1
+                        changed = True
+                color_dicts.append(same_color_neighbours)
 
-    for dict1, dict2 in iter.permutations(color_dicts, 2):
-        v = G.vertices[color_dicts.index(dict1)]
-        if dict1 != dict2:
-            v.color = n + i
+        for dict1, dict2 in iter.permutations(color_dicts, 2):
+            i = color_dicts.index(dict1)
+            v = G.vertices[i]
+            if dict1 != dict2:
+                v.colornum = n + i + 1
 
     with open("../trial.dot", 'w') as g:
         write_dot(G, g)
