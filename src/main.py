f6c30b7a3f4b51b884f16f73e6d6e67f2eff5f58
Piotrek Sikorski <gmek0204@gmail.com>
2020-03-05 14:15:12 +0100

    Correct the file loading script

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 print("Hello world")
 
-# from src.graph_io import *
-# import os
+from src.graph_io import *
+import os
 
-# cur_path = os.path.dirname(__file__)
-#
-# new_path = os.path.relpath('..\\graphs\\examplegraph.gr', cur_path)
-# with open(new_path, 'r') as f:
-#     _graph = load_graph (f)
-#     print(_graph)
+cur_path = os.path.dirname(__file__)
+
+new_path = os.path.relpath('../graphs/examplegraph.gr', cur_path)
+with open(new_path, 'r') as f:
+    _graph = load_graph (f)
+    print(_graph)
