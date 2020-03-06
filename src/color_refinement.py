10fb796af91fbd7a5b4a98b4becae489e550c09f
Joanna Rostek <joannadrostek@gmail.com>
2020-03-06 09:54:35 +0100

    new version that actually seems to work

--- src/color_refinement.py
+++ src/color_refinement.py
@@ -2,6 +2,33 @@ import itertools as iter
 
 from src.graph_io import *
 
+
+def get_coloring(G):
+    coloring = {}
+    for v in G.vertices:
+        if v.colornum not in coloring:
+            coloring[v.colornum] = v.colornum
+        else:
+            coloring[v.colornum] += 1
+    return coloring
+
+
+def have_identically_colored_neighbourhoods(u, v):
+    if u.degree != v.degree:
+        return False
+    list_of_same_colored_neighbours = [False] * u.degree
+    i = 0
+    for neighbour_u in u.neighbours:
+        for neighbour_v in v.neighbours:
+            if neighbour_u.colornum == neighbour_v.colornum and neighbour_u != neighbour_v:
+                list_of_same_colored_neighbours[i] = True
+        i += 1
+    for x in list_of_same_colored_neighbours:
+        if x is False:
+            return False
+    return True
+
+
 with open("../examplegraph.gr") as f:
     G = load_graph(f)
     n = len(G.vertices)
@@ -11,32 +38,20 @@ with open("../examplegraph.gr") as f:
 
     # initial coloring - each vertex by degree
     for v in G.vertices:
-        for i in range(1, n - 1):
-            if v.degree == i:
-                v.colornum = i
-                num_of_vertices_of_color[i] += 1
+        v.colornum = v.degree
 
-    while changed:
-        changed = False
-        # loop over the vertices of same color
-        for l in num_of_vertices_of_color:
-            if l > 1:
-                v = G.vertices[l]
-                same_color_neighbours = {}
-                for e in v.incidence:
-                    w = e.other_end(v)
-                    if w.colornum not in same_color_neighbours:
-                        same_color_neighbours[w.colornum] = 1
-                    else:
-                        same_color_neighbours[w.colornum] += 1
-                        changed = True
-                color_dicts.append(same_color_neighbours)
+    i = 0
+    while True:
+        i += 1
+        previous_coloring = get_coloring(G)
+        for u, v in iter.combinations(G.vertices, 2):
+            if u.colornum == v.colornum and have_identically_colored_neighbourhoods(u, v):
+                u.colornum = v.colornum
+            elif u.colornum == v.colornum:
+                u.colornum = n + i + 1
+        curr_coloring = get_coloring(G)
+        if previous_coloring == curr_coloring:
+            break
 
-        for dict1, dict2 in iter.permutations(color_dicts, 2):
-            i = color_dicts.index(dict1)
-            v = G.vertices[i]
-            if dict1 != dict2:
-                v.colornum = n + i + 1
-
-    with open("../trial.dot", 'w') as g:
+    with open("../color_refined_graph.dot", 'w') as g:
         write_dot(G, g)
