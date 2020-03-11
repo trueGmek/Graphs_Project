7dd17e35a3310684ccc82da9c9229e1887a5125f
Joanna Rostek <joannadrostek@gmail.com>
2020-03-11 14:02:10 +0100

    updated version

--- src/color_refinement.py
+++ src/color_refinement.py
@@ -7,7 +7,7 @@ def get_coloring(G):
     coloring = {}
     for v in G.vertices:
         if v.colornum not in coloring:
-            coloring[v.colornum] = v.colornum
+            coloring[v.colornum] = 1
         else:
             coloring[v.colornum] += 1
     return coloring
@@ -16,39 +16,44 @@ def get_coloring(G):
 def have_identically_colored_neighbourhoods(u, v):
     if u.degree != v.degree:
         return False
-    list_of_same_colored_neighbours = [False] * u.degree
-    i = 0
+    coloring_u = {}
+
+    coloring_v = {}
+
     for neighbour_u in u.neighbours:
-        for neighbour_v in v.neighbours:
-            if neighbour_u.colornum == neighbour_v.colornum and neighbour_u != neighbour_v:
-                list_of_same_colored_neighbours[i] = True
-        i += 1
-    for x in list_of_same_colored_neighbours:
-        if x is False:
-            return False
+        if neighbour_u.colornum not in coloring_u:
+            coloring_u[neighbour_u.colornum] = 1
+        else:
+            coloring_u[neighbour_u.colornum] += 1
+
+    for neighbour_v in v.neighbours:
+        if neighbour_v.colornum not in coloring_v:
+            coloring_v[neighbour_v.colornum] = 1
+        else:
+            coloring_v[neighbour_v.colornum] += 1
+    if coloring_u != coloring_v:
+        return False
     return True
 
 
 with open("../examplegraph.gr") as f:
     G = load_graph(f)
     n = len(G.vertices)
-    num_of_vertices_of_color = [0] * n
-    color_dicts = []
-    changed = True
 
     # initial coloring - each vertex by degree
     for v in G.vertices:
         v.colornum = v.degree
-
     i = 0
     while True:
         i += 1
         previous_coloring = get_coloring(G)
+        with open("../{}.dot".format(i), 'w') as g:
+            write_dot(G, g)
+        j = i
         for u, v in iter.combinations(G.vertices, 2):
-            if u.colornum == v.colornum and have_identically_colored_neighbourhoods(u, v):
-                u.colornum = v.colornum
-            elif u.colornum == v.colornum:
-                u.colornum = n + i + 1
+            if u.colornum == v.colornum and not have_identically_colored_neighbourhoods(u, v):
+                u.colornum = n + j + 1
+                j += 1
         curr_coloring = get_coloring(G)
         if previous_coloring == curr_coloring:
             break
