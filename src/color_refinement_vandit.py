bd60f06adbe6f093d3d4f4f569fc1f1833321844
trueGmek <gmek0204@gmail.com>
2020-03-22 18:33:13 +0100

    Add working version

--- src/color_refinement_vandit.py
+++ src/color_refinement_vandit.py
@@ -1,10 +1,22 @@
 from src.graph_io import *
+import os
 
-with open("examplegraph.gr") as f:
+
+def count_vertices(color):
+    i = 0
+    for v in G.vertices:
+        if v.colornum == color:
+            i += 1
+        if i > 1:
+            return True
+    return False
+
+
+path = os.path.relpath('../graphs/examplegraph.gr', os.path.dirname(__file__))
+with open(path, 'r') as f:
     G = load_graph(f)
     n = len(G.vertices)
 
-    # initial coloring - each vertex by degree
     for v in G.vertices:
         v.colornum = v.degree
 
@@ -12,50 +24,67 @@ with open("examplegraph.gr") as f:
     dic = {}
     dic_neighbour_v = {}
     lsOfSameColorVertices = []
+
     for v in G.vertices:
         if v.colornum not in all_colors:
-            all_colors.append(v.colornum)  # list containing all color numbers we have after degree refinement
+            all_colors.append(v.colornum)
 
+    different_colors = len(all_colors)
 
-    def count_vertices(color):  # define this function
-        i = 0
-        for v in G.vertices:
-            if v.colornum == color:
-                i = i + 1
-            if i > 1:
-                return True
-        return False
+    for v in G.vertices:
+        dic[v.label] = {}
 
+    for v in G.vertices:
+        for i in range(n + different_colors + 1):
+            dic[v.label][i] = 0
 
     for x in all_colors:
-        if count_vertices(x):  # if vertices with color x>=2
-            for v in G.vertices:
-                if v.colornum == x:
-                    lsOfSameColorVertices.append(v)
+        if x <= (n + different_colors):
+            lsOfSameColorVertices = []
+            if count_vertices(x):  # if vertices with color x>=2
+                for v in G.vertices:
+                    if v.colornum == x:
+                        lsOfSameColorVertices.append(v)
 
-            for v in lsOfSameColorVertices:
-                for w in v.neighbours:
-                    if v not in dic:
-                        dic[v] = {}
-                    if w.colornum not in dic[v]:
-                        dic[v][w.colornum] = 0
-                    dic[v][w.colornum] += 1
-            for key in dic:
-                if dic[key] not in dic_neighbour_v:
-                    dic_neighbour_v[dic[key]] = []
-                dic_neighbour_v[dic[key]].append(key)
+                for v in lsOfSameColorVertices:
+                    for w in v.neighbours:
+                        if w.colornum not in dic[v.label]:
+                            with open("color_refined_graph.dot", 'w') as g:
+                                write_dot(G, g)
+                            exit()
+                        else:
+                            dic[v.label][w.colornum] += 1
 
-            for key, item in dic_neighbour_v:
-                for v in item:
-                    v.colornum = max(all_colors) + 1
-                all_colors.append(max(all_colors) + 1)
+                seenArray = []
+                groupArray = []
+                # Create a new dic
+                numKey = 0
+                for v in lsOfSameColorVertices:
+                    if v.label not in seenArray:
+                        groupOfThis = [v.label]
+                        seenArray.append(v.label)
+                        for k2, val2 in dic.items():
+                            if dic[v.label] == val2 and k2 not in seenArray:
+                                groupOfThis.append(k2)
+                                seenArray.append(k2)
+                        numKey += 1
+                        groupArray.append(groupOfThis)
 
-            for v in G.vertices:
-                for i in range(max(all_colors) + 1):
-                    dic[v] = {i: 0}
+                print(all_colors)
+                print(groupArray)
+
+                for j in range(len(groupArray)):
+                    for i in groupArray[j]:
+                        for v in G.vertices:
+                            if v.label == i:
+                                v.colornum = max(all_colors) + 1
+
+                    for v in lsOfSameColorVertices:
+                        if v.colornum not in all_colors:
+                            all_colors.append(v.colornum)
+
+            else:
+                continue
 
         else:
-            continue
-
-    with open("color_refined_graph.dot", 'w') as g:
-        write_dot(G, g)
+            break
