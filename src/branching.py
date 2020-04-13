8fb421250b1433e2d157683f9fe6d73780e80c53
trueGmek <gmek0204@gmail.com>
2020-04-13 18:16:42 +0200

    Fix branching algorithm

--- src/branching.py
+++ src/branching.py
@@ -1,3 +1,6 @@
+from random import randrange
+import copy
+
 from src.color_class import ColorClass
 from src.color_refinement import *
 
@@ -8,36 +11,50 @@ This function will be called when we got stable colorings, so I will not check t
 
 def test_branching():
     graphs = read_list_of_graphs_from_file('../graphs/branching/lecture_graphs.grl')
-
     for graph in graphs:
         initialize_colornum(graph)
-    num = count_isomorphism(graphs[0], graphs[1])
-    
-    print("Number of isomorphisms: ", num)
+    print("Number of isomorphisms: ", count_isomorphism(graphs[0], graphs[1]))
 
 
 def count_isomorphism(g, h):
-    g, h = color_refinement(g), color_refinement(h)
-    if not is_balanced(g, h):
+    previous_g, previous_h = copy.deepcopy(g), copy.deepcopy(h)
+    previous_g, previous_h = color_refinement(previous_g), color_refinement(previous_h)
+    print("After refinement\n", get_coloring(previous_g), get_coloring(previous_h))
+    print("Before refinement\n", get_coloring(g), get_coloring(h))
+    if not is_balanced(previous_g, previous_h):
+        print("RETURN ZERO")
         return 0
-    if is_bijection(g, h):
+    else:
+        print("BALANCED")
+    if is_bijection(previous_g, previous_h):
+        print("RETURN ONE")
         return 1
 
+    g, h = color_refinement(g), color_refinement(h)
+    print("After checking \n", get_coloring(g), get_coloring(h))
     num = 0
     index = 0
 
     g_color_classes = list_of_colour_classes_bigger_eq_than_k(get_color_classes(g), 2)
+
     # we select the first object from the list that has all the color classes that have size greater than two
     selected_color_class = g_color_classes[index]
-    important_vertex_form_g = selected_color_class.vertices[0]  # select first vertex from first color class
 
-    for vertex in get_color_class_form_h_that_is_equal_to_input_color_class(h, selected_color_class).vertices:
+    # select first vertex from first color class
+    important_vertex_form_g = selected_color_class.vertices[randrange(len(selected_color_class.vertices))]
+    color_class_from_h = get_color_class_form_h_that_is_equal_to_input_color_class(h, selected_color_class)
+
+    for vertex in color_class_from_h.vertices:
         previous_colornum = vertex.colornum
+        previous_important_colornum = important_vertex_form_g.colornum
         important_vertex_form_g.colornum = get_highest_colornum(get_color_classes(g)) + 1
         vertex.colornum = important_vertex_form_g.colornum
+        print("FIRST loop coloring\n", get_coloring(g), get_coloring(h))
+        print("Is it balanced: ", is_balanced(g, h))
         num += count_isomorphism(g, h)
         vertex.colornum = previous_colornum
-
+        important_vertex_form_g.colornum = previous_important_colornum
+        print("SECOND loop coloring\n", get_coloring(g), get_coloring(h))
     return num
 
 
