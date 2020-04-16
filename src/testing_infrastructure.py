cf96e3d25d8ef5d7790befd560a76bd528f06348
Joanna Rostek <joannadrostek@gmail.com>
2020-04-16 10:57:23 +0200

    modified printing and added time check in cases 1 and 2

--- src/testing_infrastructure.py
+++ src/testing_infrastructure.py
@@ -8,26 +8,26 @@ from src.partition_refinement import partition_refinement
 
 FILE_NAME_GI = '../graphs/basicGI3.grl'
 FILE_NAME_AUTO = '../graphs/basicAut2.gr'
-FILE_NAME_ISO = '../graphs/basicAut1.gr'
+FILE_NAME_ISO = '../graphs/basicGIAut.grl'
 
 
 def do_the_tests():
     print_the_welcome_message()
     x = input()
+    tf = 0
     if x == '0':
         print("Sets of isomorphic graphs:")
         graphs = read_list_of_graphs_from_file(FILE_NAME_GI)
         n = len(graphs)
         previous_checks = []
-        tf = 0
         for i in range(n):
             if i in previous_checks:
                 continue
             isomorphic_graphs = [i]
             for j in range(i + 1, n):
+                t1 = time()
                 disjoint_graph = graphs[i] + graphs[j]
                 highest_colornum = set_up_colornum(disjoint_graph)
-                t1 = time()
                 if count_isomorphism([], [], disjoint_graph, highest_colornum) > 1:
                     isomorphic_graphs.append(j)
                     previous_checks.append(j)
@@ -42,27 +42,49 @@ def do_the_tests():
         graphs = read_list_of_graphs_from_file(FILE_NAME_AUTO)
         g_counter = 0
 
+        t1 = time()
         for g in graphs:
             disjoint_graph = g + g
             highest_colornum = set_up_colornum(disjoint_graph)
             print(g_counter, ":  ",
                   count_isomorphism([], [], disjoint_graph, highest_colornum))
             g_counter += 1
+        t2 = time()
+        print("Time of computation [s]: {}".format(t2 - t1))
 
     if x == '2':
         print("Sets of isomorphic graphs: Number of automorphisms: ")
         graphs = read_list_of_graphs_from_file(FILE_NAME_ISO)
-        g_counter, h_counter = 0, 0
         n = len(graphs)
+        list_of_sets = []
 
         for i in range(n):
             for j in range(i + 1, n):
+                t1 = time()
                 disjoint_graph = graphs[i] + graphs[j]
                 highest_colornum = set_up_colornum(disjoint_graph)
                 disjoint_graph = partition_refinement(disjoint_graph, len(disjoint_graph))
                 number_of_isomorphisms = count_isomorphism([], [], disjoint_graph, highest_colornum)
                 if number_of_isomorphisms > 1:
                     print('[', i, j, "]     ", number_of_isomorphisms)
+                    list_of_sets.append(i)
+                    list_of_sets.append(j)
+                t2 = time()
+                tf += (t2 - t1)
+
+        print("Graph: Number of automorphisms:")
+        g_counter = 0
+        for g in graphs:
+            t1 = time()
+            if g_counter not in list_of_sets:
+                disjoint_graph = g + g
+                highest_colornum = set_up_colornum(disjoint_graph)
+                print('[', g_counter, "]\t\t",
+                      count_isomorphism([], [], disjoint_graph, highest_colornum))
+            g_counter += 1
+            t2 = time()
+            tf += (t2 - t1)
+        print("Time of computation [s]: {}".format(tf))
 
 
 def read_list_of_graphs_from_file(path):
