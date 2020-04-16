f0ff4535bf943d51fef31f396ab155f91fd24dd3
trueGmek <gmek0204@gmail.com>
2020-04-16 10:08:54 +0200

    Improvisation

--- src/testing_infrastructure.py
+++ src/testing_infrastructure.py
@@ -1,30 +1,34 @@
 import os
-
-from src.branching import is_bijection, count_isomorphism, set_up_colornum
+from src.branching import count_isomorphism, set_up_colornum
 from src.graph import Graph
 from src.graph_io import load_graph
 from src.partition_refinement import partition_refinement
 
-FILE_NAME_GI = '../graphs/branching/products72.grl'
-FILE_NAME_AUTO = '../graphs/branching/products72.grl'
-FILE_NAME_ISO = '../graphs/branching/products72.grl'
+FILE_NAME_GI = '../basics/basicGI3.grl'
+FILE_NAME_AUTO = '../basics/basicAut1.grl'
+FILE_NAME_ISO = '../basics/basicGIAut.grl'
 
 
 def do_the_tests():
     print_the_welcome_message()
     x = input()
+
     if x == '0':
         print("Sets of isomorphic graphs:")
         graphs = read_list_of_graphs_from_file(FILE_NAME_GI)
         n = len(graphs)
-        previous_checks = {}
+        isomorphic_graphs = []
 
         for i in range(n):
+            if i in isomorphic_graphs:
+                continue
+            isomorphic_graphs = [i]
             for j in range(i + 1, n):
                 disjoint_graph = graphs[i] + graphs[j]
                 highest_colornum = set_up_colornum(disjoint_graph)
                 if count_isomorphism([], [], disjoint_graph, highest_colornum) > 1:
-                    print('[', i, j, ']')
+                    isomorphic_graphs.append(j)
+            print(str(isomorphic_graphs))
 
     if x == '1':
         print("Graph: Number of automorphisms:")
@@ -41,8 +45,8 @@ def do_the_tests():
     if x == '2':
         print("Sets of isomorphic graphs: Number of automorphisms: ")
         graphs = read_list_of_graphs_from_file(FILE_NAME_ISO)
-        g_counter, h_counter = 0, 0
         n = len(graphs)
+        list_of_sets = []
 
         for i in range(n):
             for j in range(i + 1, n):
@@ -52,6 +56,18 @@ def do_the_tests():
                 number_of_isomorphisms = count_isomorphism([], [], disjoint_graph, highest_colornum)
                 if number_of_isomorphisms > 1:
                     print('[', i, j, "]     ", number_of_isomorphisms)
+                    list_of_sets.append(i)
+                    list_of_sets.append(j)
+
+        print("Graph: Number of automorphisms:")
+        g_counter = 0
+        for g in graphs:
+            if g_counter not in list_of_sets:
+                disjoint_graph = g + g
+                highest_colornum = set_up_colornum(disjoint_graph)
+                print(g_counter, ":  ",
+                      count_isomorphism([], [], disjoint_graph, highest_colornum))
+            g_counter += 1
 
 
 def read_list_of_graphs_from_file(path):
@@ -65,4 +81,4 @@ def read_list_of_graphs_from_file(path):
 def print_the_welcome_message():
     print("For GI select: 0\n"
           "For # of Automorphisms: 1\n"
-          "For # Isomorphism between two graphs: 2")
+          "For # Isomorphism between every pair of graphs: 2")
